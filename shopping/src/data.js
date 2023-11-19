e27223f9ea8158bd4d6ff6a7d0c57819d6ecd57e
narmadavivek <nandyvivek98@gmail.com>
2023-11-19 21:30:55 +0530

    hotel

---
+++ shopping/src/data.js
@@ -0,0 +1,40 @@
+const list =[
+    {
+        id:1,
+        title:'Iphone',
+        price:40000,
+        img:'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-7inch-bluetitanium?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1692845699311'
+    },
+    {
+        id:2,
+        title:'Mac LapTop',
+        price:80000,
+        img:'https://m.media-amazon.com/images/I/41ZJJLODSAL._AC_UF1000,1000_QL80_.jpg'
+    },
+    {
+        id:3,
+        title:'Headphone',
+        price:500,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX8Og1aIH5quydtXNk-begm4kJOlQIvt3_VQ&usqp=CAU'
+    },
+    {
+        id:4,
+        title:'Andriod phone',
+        price:29000,
+        img:'https://i.gadgets360cdn.com/products/large/1536782676_635_iphone_xs_max.jpg?downsize=*:360'
+    },
+    {
+        id:5,
+        title:'bluetooth headset',
+        price:2500,
+        img:'https://images.meesho.com/images/products/128513245/3kw06_512.webp'
+    },
+    {
+        id:6,
+        title:'smartwatch',
+        price:5000,
+        img:'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ultra-band-unselect-gallery-1-202309_GEO_IN_FMT_WHH?wid=752&hei=720&fmt=p-jpg&qlt=80&.v=1693544568335'
+    },
+];
+export default list;
+    
